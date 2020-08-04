dd2047b712ecdb2275878ce68198ce23bee8a116
Enzo_Iwata <enzo.iwata8700@gmail.com>
2020-08-04 16:24:01 -0300

    comentado

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define TAM_BUFFER 60
 char buffer_entrada[TAM_BUFFER];
@@ -17,7 +18,9 @@ char subtrai_modulo_alfabeto(char senha, char codigo){
     if(codigo >= 'A' && codigo <= 'Z'){
         senha0 = 'A' - senha;
         codigo0 = 'A' - codigo;
-
+        /*subtraio o caractere da senha pelo do codigo pra gerar o caractere que buscamos
+          ja que pra gerar a cifra somamos o caractere da senha pelo texto, a soma de 'Z'-'A'+1
+          se da para que resultados negativos da subtracao sejam positivos(deslocamento em 'Z'-'A'+1 para direita)*/
         original0 = 'A' + (('Z'-'A'+ 1 + senha0 - codigo0)%('Z'-'A'+1));
         return original0;    
     }else return codigo;
@@ -31,10 +34,17 @@ int main() {
     fgets(buffer_entrada, TAM_BUFFER, stdin);
     
     for(i = 0; (i < TAM_BUFFER) && (buffer_entrada[i] != '\0') && (buffer_entrada[i] != '\n'); i++){
+        /*coloca os caracteres para uppercase(mais pra testes do que pro lab mesmo pra poder entrar
+          com caracteres em letra minuscula)*/
+        buffer_entrada[i] = toupper(buffer_entrada[i]);
+        /*Decodifica o codigo e coloca no buffer de saida*/
         buffer_saida[i] = subtrai_modulo_alfabeto(senha[j], buffer_entrada[i]);
         j++;
+        /*rotacao da senha para ficar algo como SENHASECRETASENHASECRETA se o tamanho da entrada
+          for maior que o tamanho da senha*/
         if(senha[j] == '\0') j = 0;
     }
+    /*insere o caractere de fim de string na saida*/
     buffer_saida[i] = '\0';
 
 
